01cd97a77d3e3f600cc8191c468e6d8267c3a09a
abdur-rahman <arafridi1232@gmail.com>
2025-08-21 18:49:17 +0500

    conflect reslove

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'drupal/recommended-project',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'ff9f8e51a60bb48f8aa81b9f1cd4975151f9acd0',
+        'reference' => '2f511938cf35e6e0995faa562062d39077db4ad3',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -418,7 +418,7 @@
         'drupal/recommended-project' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'ff9f8e51a60bb48f8aa81b9f1cd4975151f9acd0',
+            'reference' => '2f511938cf35e6e0995faa562062d39077db4ad3',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
