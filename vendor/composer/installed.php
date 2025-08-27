055ad72d6018797ca247cca85159531bb42efc88
abdur-rahman <arafridi1232@gmail.com>
2025-08-27 18:00:49 +0500

    add all the pages content

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'drupal/recommended-project',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '97d1e165dec237e60aa96e5dee882828ae948a44',
+        'reference' => '02e94a57b4aa2613eab4205d98ed305c7a619df2',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -481,7 +481,7 @@
         'drupal/recommended-project' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '97d1e165dec237e60aa96e5dee882828ae948a44',
+            'reference' => '02e94a57b4aa2613eab4205d98ed305c7a619df2',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -775,8 +775,8 @@
         'psr/container-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.1|2.0',
-                1 => '^1.0',
+                0 => '^1.0',
+                1 => '1.1|2.0',
             ),
         ),
         'psr/event-dispatcher' => array(
