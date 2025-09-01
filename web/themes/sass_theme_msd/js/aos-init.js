785535c2e5f8eccde864373fa5e4b47d0457970d
abdur-rahman <arafridi1232@gmail.com>
2025-09-01 10:02:44 +0500

    add all the pages content

--- web/themes/sass_theme_msd/js/aos-init.js
+++ web/themes/sass_theme_msd/js/aos-init.js
@@ -2,9 +2,9 @@
   Drupal.behaviors.aosInit = {
     attach: function (context, settings) {
       AOS.init({
-        duration: 1200, // speed
-        once: false,    // animate every time
-        mirror: true    // also on scroll up
+        duration: 1200,
+        once: false, 
+        mirror: true,   
       });
     }
   };
